74541d7f37df8e60b43010cff0e5d940bd46f1e6
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-11-16 11:56:43 +0530

    Update Button_test.py

--- IoTFi_2G/Button_test.py
+++ IoTFi_2G/Button_test.py
@@ -1,3 +1,9 @@
+'''
+This is the example code for working with additional GPIO's  functionality
+Develp by sbcomponents
+
+'''
+
 from machine import Pin
 import time
 
@@ -6,7 +12,12 @@ import time
 b1 = Pin(22, Pin.IN, Pin.PULL_UP)
 b2 = Pin(23,Pin.IN, Pin.PULL_UP)
 
+GP15 = Pin(15,Pin.OUT)
+GP26 = Pin(26,Pin.OUT)
 while (1):
+    GP15.value(1)
+    GP26.value(1)
+    
     val1 = b1.value()
     val2 = b2.value()
     
